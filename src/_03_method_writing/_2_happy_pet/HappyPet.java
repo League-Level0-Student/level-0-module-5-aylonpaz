dae706c567794e6597282921088b5c75a7c9b56b
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2026-01-16 18:17:25 -0800

    OFJIOSFHPS(TE(FGPUVSGtbwzeo 4cou

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,19 +5,32 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	int  happenessLevel = 0;
+	static int  happenessLevel = 0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-		String pet = JOptionPane.showInputDialog("What pet do you want? Fish, cat, dog, or felony tax "
+		String pet = JOptionPane.showInputDialog("What pet do you want? Fish, cat, dog, or tax "
 				+ "evasion?");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
-		
+
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "What would you like to do?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Walk", "Pet", "Feed" }, null);
-
+				if(pet.toLowerCase().contains("tax")&& task == 0) {
+					happenessLevel -= 10000;
+				}
+				else if(pet.toLowerCase().contains("tax") && task == 1) {
+					happenessLevel -= 10000000;
+				}
+				else if(pet.toLowerCase().contains("tax") && task == 2) {
+					happenessLevel -= 1000000000;
+				}
+						
+				if(happenessLevel < 0) {
+					JOptionPane.showMessageDialog(null, "YOU SUCK! :O");
+					return;
+				}
 			// 6. Use user input to call the appropriate method created in step 5 below.
 		
 				
@@ -25,9 +38,9 @@ public class HappyPet {
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
-	}
-
+	{
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+}
 }
