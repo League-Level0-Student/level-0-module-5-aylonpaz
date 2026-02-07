cd08198a94b48e15aa396025904af58009e1d1dd
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2026-02-06 18:23:34 -0800

    dsfa

--- src/_04_practice/_1_skill_practice/TheChickenItsEatingMeFromTheInside.java
+++ src/_04_practice/_1_skill_practice/TheChickenItsEatingMeFromTheInside.java
@@ -1,11 +1,49 @@
 package _04_practice._1_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class TheChickenItsEatingMeFromTheInside {
 public static void main(String[]args) {
 	String pneumonoultramicroscopicsilicovolcanoconiosis = JOptionPane.showInputDialog("How many dime u got???");
 	int hippopotomonstrosesquippedaliophobia =Integer.parseInt(pneumonoultramicroscopicsilicovolcanoconiosis);
-	
+	int dimeeeeeeeeeeeeeeeeeeeeeeeeees = hippopotomonstrosesquippedaliophobia * 10;
+	JOptionPane.showMessageDialog(null, "You have " + dimeeeeeeeeeeeeeeeeeeeeeeeeees + " cents"
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ""
+			+ ".");
+	String  antidisestablishmentarianism = JOptionPane.showInputDialog("How tall are you??????????");
+	int disestablishmentarianism = Integer.parseInt(antidisestablishmentarianism); 
+	int dusestablishmentarianism2 = disestablishmentarianism * 12;
+	if(disestablishmentarianism <= 36) {
+		JOptionPane.showMessageDialog(null, "DWARF LMAO");
+	}
+	for(int ahhhhhhh = 1; ahhhhhhh <= 30; ahhhhhhh +=1) {
+		if( ahhhhhhh % 3 == 0) {
+			System.out.println(ahhhhhhh);
+		}
+			}
+	Random sdf = new Random();
+	int feawifaeniaew = sdf.nextInt(20);
+	System.out.println(feawifaeniaew);
+	Random imdonewiththelongvariablenames = new Random();
+	int hehe = imdonewiththelongvariablenames.nextInt(10);
+	System.out.println(hehe);
+	int j = sdf - hehe;
+	JOptionPane.showMessageDialog();
 }
 }
